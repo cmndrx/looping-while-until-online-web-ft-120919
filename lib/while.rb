a82026e685b3ea8f8357c4b7ffe7eb6e8852542d
Autonyo Sanders <tonio.sanders@gmail.com>
2019-12-11 18:16:24 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,11 @@
 def using_while
   levitation_force = 6
 	
-  #write match
   while levitation_force < 10
-  levitation_force
+  levitation_force +=1
   puts "Wingardium Leviosa"
+  end
+  
 end
 
 
